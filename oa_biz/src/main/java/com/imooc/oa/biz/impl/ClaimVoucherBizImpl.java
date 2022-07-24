db7d04f021220c98b3b2dc9f6f367e89253d2221
Charmalyu <929638617@qq.com>
2022-07-24 16:48:14 +0800

    oa

---
+++ oa_biz/src/main/java/com/imooc/oa/biz/impl/ClaimVoucherBizImpl.java
@@ -0,0 +1,152 @@
+package com.imooc.oa.biz.impl;
+
+import com.imooc.oa.biz.ClaimVoucherBiz;
+import com.imooc.oa.dao.ClaimVoucherDao;
+import com.imooc.oa.dao.ClaimVoucherItemDao;
+import com.imooc.oa.dao.DealRecordDao;
+import com.imooc.oa.dao.EmployeeDao;
+import com.imooc.oa.entity.ClaimVoucher;
+import com.imooc.oa.entity.ClaimVoucherItem;
+import com.imooc.oa.entity.DealRecord;
+import com.imooc.oa.entity.Employee;
+import com.imooc.oa.global.Contant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+@Service("claimVoucherBiz")
+public class ClaimVoucherBizImpl implements ClaimVoucherBiz {
+    @Autowired
+    private ClaimVoucherDao claimVoucherDao;
+    @Autowired
+    private ClaimVoucherItemDao claimVoucherItemDao;
+    @Autowired
+    private DealRecordDao dealRecordDao;
+    @Autowired
+    private EmployeeDao employeeDao;
+
+    public void save(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items) {
+        claimVoucher.setCreateTime(new Date());
+        claimVoucher.setNextDealSn(claimVoucher.getCreateSn());
+        claimVoucher.setStatus(Contant.CLAIMVOUCHER_CREATED);
+        claimVoucherDao.insert(claimVoucher);
+
+        for (ClaimVoucherItem item:items){
+            item.setClaimVoucherId(claimVoucher.getId());
+            claimVoucherItemDao.insert(item);
+        }
+    }
+
+    public ClaimVoucher get(int id) {
+        return claimVoucherDao.select(id);
+    }
+
+    public List<ClaimVoucherItem> getItems(int cvid) {
+        return claimVoucherItemDao.selectByClaimVoucher(cvid);
+    }
+
+    public List<DealRecord> getRecords(int cvid) {
+        return dealRecordDao.selectByClaimVoucher(cvid);
+    }
+
+    public List<ClaimVoucher> getForSelf(String sn) {
+        return claimVoucherDao.selectByCreateSn(sn);
+    }
+
+    public List<ClaimVoucher> getForDeal(String sn) {
+        return claimVoucherDao.selectByNextDealSn(sn);
+    }
+
+    public void update(ClaimVoucher claimVoucher, List<ClaimVoucherItem> items) {
+        claimVoucher.setNextDealSn(claimVoucher.getCreateSn());
+        claimVoucher.setStatus(Contant.CLAIMVOUCHER_CREATED);
+        claimVoucherDao.update(claimVoucher);
+
+        /*更新条目信息*/
+        /*获取当前数据库里已有的条目信息*/
+        List<ClaimVoucherItem> olds = claimVoucherItemDao.selectByClaimVoucher(claimVoucher.getId());
+        /*迭代这个集合,判断这个old是不是不存在items里*/
+        for (ClaimVoucherItem old: olds){
+            /*先判定不存在*/
+            boolean isHave=false;
+            /*迭代现在的，准备要更新的条目集合*/
+            for (ClaimVoucherItem item:items){
+                /*如果相等代表存在*/
+                if (item.getId()==old.getId()){
+                    isHave=true;
+                    break;
+                }
+            }
+            /*如果不等于false,也就是存在，就把old这个条目删除*/
+            if(!isHave){
+                claimVoucherItemDao.delete(old.getId());
+            }
+        }
+        /*如果不存在或者已删除就迭代新的条目*/
+        for (ClaimVoucherItem item:items){
+            /*为item设置报销单id，否则修改数据时新增item会发生空指针异常*/
+            item.setClaimVoucherId(claimVoucher.getId());
+            /*如果这个条目大于0，说明数据库之前有过这个条目，所以要更新内容*/
+            if (item.getId()!=null && item.getId()>0){
+                claimVoucherItemDao.update(item);
+            }else {
+                claimVoucherItemDao.insert(item);
+            }
+        }
+    }
+
+    public void submit(int id) {
+        ClaimVoucher claimVoucher = claimVoucherDao.select(id);
+        Employee employee = employeeDao.select(claimVoucher.getCreateSn());
+        claimVoucher.setStatus(Contant.CLAIMVOUCHER_SUBMIT);
+        claimVoucher.setNextDealSn(employeeDao.selectByDepartmentAndPost(employee.getDepartmentSn(), Contant.POST_FM).get(0).getSn());
+        claimVoucherDao.update(claimVoucher);
+
+        DealRecord dealRecord = new DealRecord();
+        dealRecord.setDealWay(Contant.DEAL_SUBMIT);
+        dealRecord.setDealSn(employee.getSn());
+        dealRecord.setClaimVoucherId(id);
+        dealRecord.setDealResult(Contant.CLAIMVOUCHER_SUBMIT);
+        dealRecord.setComment("无");
+        dealRecordDao.insert(dealRecord);
+    }
+
+    public void deal(DealRecord dealRecord) {
+        ClaimVoucher claimVoucher = claimVoucherDao.select(dealRecord.getClaimVoucherId());
+        Employee employee = employeeDao.select(dealRecord.getDealSn());
+        dealRecord.setDealTime(new Date());
+
+        if(dealRecord.getDealWay().equals(Contant.DEAL_PASS)){
+            if(claimVoucher.getTotalAmount()<=Contant.LIMIT_CHECK || employee.getPost().equals(Contant.POST_GM)){
+                claimVoucher.setStatus(Contant.CLAIMVOUCHER_APPROVED);
+                claimVoucher.setNextDealSn(employeeDao.selectByDepartmentAndPost(null,Contant.POST_CASHIER).get(0).getSn());
+
+                dealRecord.setDealResult(Contant.CLAIMVOUCHER_APPROVED);
+            }else{
+                claimVoucher.setStatus(Contant.CLAIMVOUCHER_RECHECK);
+                claimVoucher.setNextDealSn(employeeDao.selectByDepartmentAndPost(null,Contant.POST_GM).get(0).getSn());
+
+                dealRecord.setDealResult(Contant.CLAIMVOUCHER_RECHECK);
+            }
+        }else if(dealRecord.getDealWay().equals(Contant.DEAL_BACK)){
+            claimVoucher.setStatus(Contant.CLAIMVOUCHER_BACK);
+            claimVoucher.setNextDealSn(claimVoucher.getCreateSn());
+
+            dealRecord.setDealResult(Contant.CLAIMVOUCHER_BACK);
+        }else if(dealRecord.getDealWay().equals(Contant.DEAL_REJECT)){
+            claimVoucher.setStatus(Contant.CLAIMVOUCHER_TERMINATED);
+            claimVoucher.setNextDealSn(null);
+
+            dealRecord.setDealResult(Contant.CLAIMVOUCHER_TERMINATED);
+        }else if(dealRecord.getDealWay().equals(Contant.DEAL_PAID)){
+            claimVoucher.setStatus(Contant.CLAIMVOUCHER_PAID);
+            claimVoucher.setNextDealSn(null);
+
+            dealRecord.setDealResult(Contant.CLAIMVOUCHER_PAID);
+        }
+
+        claimVoucherDao.update(claimVoucher);
+        dealRecordDao.insert(dealRecord);
+    }
+}
---
+++ oa_dao/src/main/java/com/imooc/oa/dao/ClaimVoucherDao.java
@@ -0,0 +1,14 @@
+package com.imooc.oa.dao;
+
+import com.imooc.oa.entity.ClaimVoucher;
+
+import java.util.List;
+
+public interface ClaimVoucherDao {
+    void insert(ClaimVoucher claimVoucher);
+    void update(ClaimVoucher claimVoucher);
+    void delete(int id);
+    ClaimVoucher select(int id);
+    List<ClaimVoucher> selectByCreateSn(String csn);
+    List<ClaimVoucher> selectByNextDealSn(String ndsn);
+}
